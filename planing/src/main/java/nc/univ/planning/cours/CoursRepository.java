28907f55d2c55271273c0e00257051966b9829da
jas0nMahoux <56009900+jas0nMahoux@users.noreply.github.com>
2022-06-17 16:01:24 +1100

    trier cours par salle

--- planing/src/main/java/nc/univ/planning/cours/CoursRepository.java
+++ planing/src/main/java/nc/univ/planning/cours/CoursRepository.java
@@ -1,8 +1,10 @@
 package nc.univ.planning.cours;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,7 @@ public interface CoursRepository extends JpaRepository<Cours, Long> {
     void deleteCoursById(Long id);
 
     Optional<Object> findCoursById(Long id);
+
+    List<Cours> findAllCoursByOrderBySalleCode();
+
 }
